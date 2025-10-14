8004f12f30575edf0f7ca9d2c384b7d6fa51cb9b
uditplayz10 <uditplayz10@gmail.com>
2025-10-14 15:50:17 +0000

    switched cdn to lib

--- js/topics/PlanesProjection.js
+++ js/topics/PlanesProjection.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.0/build/three.module.js';
+import * as THREE from '../../libs/three.module.js';
 
 class PlanesProjection {
     constructor(sceneManager, uiManager) {
